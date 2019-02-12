93d0e06483529cb1614ef7f49d0f5769522c5eb6
Colleen Lau <clau23@calpoly.edu>
2019-02-12 11:59:28 -0800

    Pcv park now toggles through on and off while cycling through floors

--- pcv_park.py
+++ pcv_park.py
@@ -3,6 +3,7 @@
 from microbit import *
 
 index = 0
+on = True
 while True:
     # display.scroll('Hello, World!')
     # display.show(Image.HEART)
@@ -12,21 +13,24 @@ while True:
     #index keeps track of what floor
     #accelerometer very junk :/
     #pin 0 + gnd hold microbit is weak
-    on = True
     pcv = ['1', '2', '3', '4', '5', '6']
     if button_a.is_pressed():
         index = index - 1
         if index < 0 :
-            index = 4
+            index = 5
         display.show(pcv[index])
         sleep(300) #500 milliseconds
         
     if button_b.is_pressed():
         on = not on
-        if on == False:
-            display.off()
-        else:
+        if on == True:
             display.on()
+            display.show(pcv[index])
+            sleep(200)
+        else:
+            display.scroll('off')
+            display.off()
+            sleep(200)
         #for plus incrementing with button B
         #display.scroll("2!")
         # index += 1
